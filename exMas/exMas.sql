f48ba6e75080112e998f301953aa2a0921f50d60
EYOOOX <85031213+EYOOOX@users.noreply.github.com>
2021-11-28 16:20:13 +0100

    Update exMas.sql

--- exMas/exMas.sql
+++ exMas/exMas.sql
@@ -1,5 +1,7 @@
 INSERT INTO `items` (`name`, `label`, `limit`) VALUES       ('christmashat', 'Bonnet de Noël', 5);
+INSERT INTO `items` (`name`, `label`, `limit`) VALUES       ('christmashorn', 'Corne de Rennes', 5);
 
 -- LEGACY
 
 INSERT INTO `items` (`name`, `label`, `weight`) VALUES       ('christmashat', 'Bonnet de Noël', 0.5);
+INSERT INTO `items` (`name`, `label`, `weight`) VALUES       ('christmashorn', 'Corne de Rennes', 0.5);
